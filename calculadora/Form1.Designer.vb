83f9c718ac943d00538beffccee65faa04dafff4
Sergio Condori Baldiviezo <go.gio.king.sc@gmail.com>
2020-07-16 21:49:05 -0400

    Agregar archivos de proyecto.

---
+++ calculadora/Form1.Designer.vb
@@ -0,0 +1,323 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtpantalla = New System.Windows.Forms.TextBox()
+        Me.btnborrartodo = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btndivision = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btnmultiplicacion = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btnigual = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btnpunto = New System.Windows.Forms.Button()
+        Me.btnmas = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnpotencia = New System.Windows.Forms.Button()
+        Me.btnmenos = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtpantalla
+        '
+        Me.txtpantalla.Font = New System.Drawing.Font("Consolas", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtpantalla.Location = New System.Drawing.Point(12, 12)
+        Me.txtpantalla.Multiline = True
+        Me.txtpantalla.Name = "txtpantalla"
+        Me.txtpantalla.Size = New System.Drawing.Size(155, 33)
+        Me.txtpantalla.TabIndex = 0
+        Me.txtpantalla.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btnborrartodo
+        '
+        Me.btnborrartodo.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btnborrartodo.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnborrartodo.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btnborrartodo.Location = New System.Drawing.Point(12, 60)
+        Me.btnborrartodo.Name = "btnborrartodo"
+        Me.btnborrartodo.Size = New System.Drawing.Size(32, 37)
+        Me.btnborrartodo.TabIndex = 4
+        Me.btnborrartodo.Text = "CE"
+        Me.btnborrartodo.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btn8.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn8.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btn8.Location = New System.Drawing.Point(50, 103)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(32, 31)
+        Me.btn8.TabIndex = 8
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'btndivision
+        '
+        Me.btndivision.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btndivision.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btndivision.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btndivision.Location = New System.Drawing.Point(88, 60)
+        Me.btndivision.Name = "btndivision"
+        Me.btndivision.Size = New System.Drawing.Size(36, 37)
+        Me.btndivision.TabIndex = 7
+        Me.btndivision.Text = "/"
+        Me.btndivision.UseVisualStyleBackColor = False
+        '
+        'btn9
+        '
+        Me.btn9.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btn9.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn9.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btn9.Location = New System.Drawing.Point(88, 103)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(36, 31)
+        Me.btn9.TabIndex = 6
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btn7.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn7.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btn7.Location = New System.Drawing.Point(12, 103)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(32, 32)
+        Me.btn7.TabIndex = 5
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btn5.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn5.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btn5.Location = New System.Drawing.Point(50, 141)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(32, 30)
+        Me.btn5.TabIndex = 12
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'btnmultiplicacion
+        '
+        Me.btnmultiplicacion.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btnmultiplicacion.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnmultiplicacion.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btnmultiplicacion.Location = New System.Drawing.Point(50, 60)
+        Me.btnmultiplicacion.Name = "btnmultiplicacion"
+        Me.btnmultiplicacion.Size = New System.Drawing.Size(32, 37)
+        Me.btnmultiplicacion.TabIndex = 11
+        Me.btnmultiplicacion.Text = "*"
+        Me.btnmultiplicacion.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btn6.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn6.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btn6.Location = New System.Drawing.Point(88, 140)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(36, 31)
+        Me.btn6.TabIndex = 10
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btn4.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btn4.Location = New System.Drawing.Point(12, 141)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(32, 29)
+        Me.btn4.TabIndex = 9
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btn2.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btn2.Location = New System.Drawing.Point(50, 176)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(32, 30)
+        Me.btn2.TabIndex = 16
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'btnigual
+        '
+        Me.btnigual.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btnigual.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnigual.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btnigual.Location = New System.Drawing.Point(130, 176)
+        Me.btnigual.Name = "btnigual"
+        Me.btnigual.Size = New System.Drawing.Size(37, 67)
+        Me.btnigual.TabIndex = 15
+        Me.btnigual.Text = "="
+        Me.btnigual.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btn3.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btn3.Location = New System.Drawing.Point(88, 176)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(36, 30)
+        Me.btn3.TabIndex = 14
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btn1.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btn1.Location = New System.Drawing.Point(12, 176)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(32, 31)
+        Me.btn1.TabIndex = 13
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btnpunto
+        '
+        Me.btnpunto.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btnpunto.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnpunto.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btnpunto.Location = New System.Drawing.Point(88, 212)
+        Me.btnpunto.Name = "btnpunto"
+        Me.btnpunto.Size = New System.Drawing.Size(36, 31)
+        Me.btnpunto.TabIndex = 20
+        Me.btnpunto.Text = "."
+        Me.btnpunto.UseVisualStyleBackColor = False
+        '
+        'btnmas
+        '
+        Me.btnmas.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btnmas.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnmas.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btnmas.Location = New System.Drawing.Point(130, 60)
+        Me.btnmas.Name = "btnmas"
+        Me.btnmas.Size = New System.Drawing.Size(37, 37)
+        Me.btnmas.TabIndex = 18
+        Me.btnmas.Text = "+"
+        Me.btnmas.UseVisualStyleBackColor = False
+        '
+        'btn0
+        '
+        Me.btn0.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btn0.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn0.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btn0.Location = New System.Drawing.Point(12, 213)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(70, 30)
+        Me.btn0.TabIndex = 17
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = False
+        '
+        'btnpotencia
+        '
+        Me.btnpotencia.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btnpotencia.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnpotencia.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btnpotencia.Location = New System.Drawing.Point(130, 139)
+        Me.btnpotencia.Name = "btnpotencia"
+        Me.btnpotencia.Size = New System.Drawing.Size(37, 31)
+        Me.btnpotencia.TabIndex = 22
+        Me.btnpotencia.Text = "^"
+        Me.btnpotencia.UseVisualStyleBackColor = False
+        '
+        'btnmenos
+        '
+        Me.btnmenos.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btnmenos.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnmenos.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btnmenos.Location = New System.Drawing.Point(130, 103)
+        Me.btnmenos.Name = "btnmenos"
+        Me.btnmenos.Size = New System.Drawing.Size(37, 32)
+        Me.btnmenos.TabIndex = 23
+        Me.btnmenos.Text = "-"
+        Me.btnmenos.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(180, 256)
+        Me.Controls.Add(Me.btnmenos)
+        Me.Controls.Add(Me.btnpotencia)
+        Me.Controls.Add(Me.btnpunto)
+        Me.Controls.Add(Me.btnmas)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btnigual)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btnmultiplicacion)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btndivision)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btnborrartodo)
+        Me.Controls.Add(Me.txtpantalla)
+        Me.Name = "Form1"
+        Me.Text = "Calculadora Infija"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtpantalla As System.Windows.Forms.TextBox
+    Friend WithEvents btnborrartodo As System.Windows.Forms.Button
+    Friend WithEvents btn8 As System.Windows.Forms.Button
+    Friend WithEvents btndivision As System.Windows.Forms.Button
+    Friend WithEvents btn9 As System.Windows.Forms.Button
+    Friend WithEvents btn7 As System.Windows.Forms.Button
+    Friend WithEvents btn5 As System.Windows.Forms.Button
+    Friend WithEvents btnmultiplicacion As System.Windows.Forms.Button
+    Friend WithEvents btn6 As System.Windows.Forms.Button
+    Friend WithEvents btn4 As System.Windows.Forms.Button
+    Friend WithEvents btn2 As System.Windows.Forms.Button
+    Friend WithEvents btnigual As System.Windows.Forms.Button
+    Friend WithEvents btn3 As System.Windows.Forms.Button
+    Friend WithEvents btn1 As System.Windows.Forms.Button
+    Friend WithEvents btnpunto As System.Windows.Forms.Button
+    Friend WithEvents btnmas As System.Windows.Forms.Button
+    Friend WithEvents btn0 As System.Windows.Forms.Button
+    Friend WithEvents btnpotencia As System.Windows.Forms.Button
+    Friend WithEvents btnmenos As System.Windows.Forms.Button
+
+End Class
